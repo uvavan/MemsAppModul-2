4ab5e8a3f7d97923bd7bc69582189b676c933f0e
Admin <vitaliy_kazakoff@mail.ru>
2018-01-12 12:51:27 +0200

    Add Valid Email

--- MemesApp/Controller/LoginViewController.swift
+++ MemesApp/Controller/LoginViewController.swift
@@ -10,28 +10,64 @@ import UIKit
 import KeychainSwift
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet private weak var ibLoginTextField: UITextField!
+    
     private let keychain = KeychainSwift()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        let string  = keychain.get(KeyNames.LoginName)
+        ibLoginTextField.text = nil
         if keychain.get(KeyNames.LoginName) != nil {
             performSegue(withIdentifier: IdentifierName.ShowMemeScrin, sender: nil)
         }
     }
-
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        guard segue.identifier == IdentifierName.ShowMemeScrin, ((segue.destination as? MemesScrinViewController) != nil) else { return }
-//    }
+    
+    private func setupView() {
+        ibLoginTextField.delegate = self
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectRecognizerTap(_:)))
+        view.addGestureRecognizer(tapRecognizer)
+    }
+    
+    @objc private func selectRecognizerTap(_ sender: UITapGestureRecognizer) {
+        hideKeyboard()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard segue.identifier == IdentifierName.ShowMemeScrin, ((segue.destination as? MemesScrinViewController) != nil) else { return }
+    }
     
     @IBAction func ibStartButtonPush(_ sender: Any) {
         guard let login = ibLoginTextField.text else {return}
+        guard !login.isEmpty else {return}
+        guard login.isValidEmail else {return}
         keychain.set(login, forKey: KeyNames.LoginName)
+        performSegue(withIdentifier: IdentifierName.ShowMemeScrin, sender: nil)
+    }
+    
+    private func hideKeyboard() {
+        view.endEditing(true)
+    }
+    
+}
+
+extension String {
+    var isValidEmail: Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: self)
+    }
+}
+
+// MARK: - UITextFieldDelegate
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        hideKeyboard()
+        return true
     }
 }
--- MemesApp/KeyNames.swift
+++ MemesApp/KeyNames.swift
@@ -10,5 +10,5 @@ import Foundation
 
 enum KeyNames {
     static let LoginName = "LoginName"
+    static let LoginList = "LoginList"
 }
-
